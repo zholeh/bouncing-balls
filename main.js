6c98a1cfadc61b27aae659c256e74aece5c41882
Oleh <zholeg@ukr.net>
2018-05-16 20:14:31 +0300

    add mocha test

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ var ctx = canvas.getContext('2d');
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 
-// function to generate random number
-
 function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
